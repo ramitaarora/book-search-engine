c595f71de7fe34ad16d8f614df604f037f4846cd
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-15 09:01:37 -0800

    mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,22 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-    mutation login($email: String, $password: String!, $username: String) {
-        login(email: $email, username: $username, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
+    mutation login($username: String, $email: String, $password: String!) {
+        login(username: $username, email: $email, password: $password) {
+        token
+        user {
+            username
+        }
         }
     }
 `;
 
 export const ADD_USER = gql`
-    mutation createUser($username: String!, $email: String!, password: String!) {
-        createUser(username: $username, email: $email: password: $password) {
-            token
-            user {
-                _id
-                username
-            }
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+        _id
+        email
+        username
         }
     }
 `;
@@ -27,10 +24,11 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
     mutation saveBook($userId: ID!, $body: String!) {
         saveBook(userId: $userId, body: $body) {
-            user {
-                _id
-                savedBooks
-            }
+        savedBooks {
+            bookId
+            title
+        }
+        _id
         }
     }
 `;
@@ -38,10 +36,10 @@ export const SAVE_BOOK = gql`
 export const DELETE_BOOK = gql`
     mutation deleteBook($userId: ID!, $bookId: String!) {
         deleteBook(userId: $userId, bookId: $bookId) {
-            user {
-                _id
-                savedBooks
-            }
+        savedBooks {
+            bookId
+            title
+        }
         }
     }
 `;
