eace52bba9d6ce2bff7b58dcf2d24f6a158cc339
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-17 08:55:28 -0800

    typo

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-    query getSingleUser($id: ID, $username: String) {
-        getSingleUser(_id: $id, username: $username) {
-        email
-        username
-        savedBooks {
-            title
-            authors
-        }
-        }
-    }
-`
-
 export const GET_ME = gql `
     query me {
         me {
